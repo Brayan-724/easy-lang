1789bca8e52408db0d11ea12c6d7c09276077b5a
Brayan-724 <bjqf0724@gmail.com>
2022-06-30 19:15:48 -0700

    (sintaxer) initial

---
+++ packages/easy-sintaxer/scripts/all-exports.ts
@@ -0,0 +1,21 @@
+import {dirname, join} from "node:path";
+import {addExport} from "./add-export";
+
+const [_, binPath, debug] = process.argv;
+
+const allExports: Record<string, string> = {};
+
+async function main() {
+  for (const [name, exports] of Object.entries(allExports)) {
+    for (const exportName of exports) {
+      await addExport(
+        join(dirname(binPath), ".."),
+        name,
+        exportName,
+        debug === "true" || Boolean(debug),
+      );
+    }
+  }
+}
+
+main();
